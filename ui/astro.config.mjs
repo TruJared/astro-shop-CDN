e454a76ea3e7d89ad0bd0888065c07ab944bdf8d
Jared Truscott <jtruscott78@gmail.com>
2023-05-28 08:24:48 -0400

    initial commit

---
+++ ui/astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import prefetch from "@astrojs/prefetch";
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react(), prefetch(), image()]
+});
---
+++ ui/tailwind.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.stone,
+      primary: colors.orange,
+      secondary: colors.lime,
+      accent: colors.sky,
+      accentAlt: colors.yellow,
+      success: colors.green,
+      warning: colors.yellow,
+      error: colors.red,
+      info: colors.blue,
+    },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
